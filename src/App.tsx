e66f9901f283515134847ced808090aa461e9225
annawilczura <98234888+annawilczura@users.noreply.github.com>
2025-10-23 13:23:47 +0200

    feat: display number of list items

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ export function App() {
           data-testid="todo-count"
           className="text-sm font-medium leading-6 text-gray-900"
         >
-          0 items left
+          {todos.length} items left
         </span>
         <button className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Clear completed
